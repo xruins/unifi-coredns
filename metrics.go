5af1b5b17a3e5bc45a9cba757a4063f35fbf06e1
Ruins <sv@ruins.mobi>
2023-11-08 03:04:31 +0900

    fix subsystem name of metrics

--- metrics.go
+++ metrics.go
@@ -11,14 +11,14 @@ var (
 	// unifiHostsEntries is the combined number of entries.
 	unifiHostsEntries = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: plugin.Namespace,
-		Subsystem: "hosts",
+		Subsystem: "unifi",
 		Name:      "entries",
 		Help:      "The combined number of entries in hosts and Corefile.",
 	}, []string{})
 	// hostsReloadTime is the timestamp of the last reload of the records by Unifi API.
 	unifiHostsReloadTime = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: plugin.Namespace,
-		Subsystem: "hosts",
+		Subsystem: "unifi",
 		Name:      "reload_timestamp_seconds",
 		Help:      "The timestamp of the last reload of hosts file.",
 	})
